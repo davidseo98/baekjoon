ee2961dc503032eff4f735b133a49c515ab30f21
hyunduk <hyundukseo@naver.com>
2022-03-22 13:36:29 +0900

    dfs silver 2 1012 solved

--- graph/dfs/1012.py
+++ graph/dfs/1012.py
@@ -1,28 +1,35 @@
-t = int(input())
-for i in range(t) :
-    m,n,l = map(int, input().split())
-    graph = [[0 for _ in range(n)] for _ in range(m)]
-    for i in range(l) :
-        x, y = map(int, input().split())
-        graph[x][y] = 1
-
-def dfs(graph, x, y) :
+import sys
+sys.setrecursionlimit(10000)
+def dfs(x, y) :
     if x <= -1 or y <= -1 or x >= m or y >= n :
         return False
     if graph[x][y] == 0 :
         return False
     graph[x][y] = 0
-    dfs(graph,x-1,y)
-    dfs(graph,x+1,y)
-    dfs(graph,x,y-1)
-    dfs(graph,x,y+1)
+    dfs(x-1,y)
+    dfs(x+1,y)
+    dfs(x,y-1)
+    dfs(x,y+1)
     return True
 
-answer = 0
-for i in range(m) :
-    for j in range(n) :
-        if dfs(graph, i, j) is True :
-            answer +=1
+t = int(input())
+answers = []
+for i in range(t) :
+    n,m,l = map(int, input().split())
+    graph = [[0]*n for _ in range(m)]
+    for i in range(l) :
+        x, y = map(int, input().split())
+        graph[y][x] = 1
 
-print(answer)
+    answer = 0
+    for i in range(m) :
+        for j in range(n) :
+            if graph[i][j]==1 :
+                dfs(i,j)
+                answer +=1
+    answers.append(answer)
+
+
+for answer in answers : 
+    print(answer)
         

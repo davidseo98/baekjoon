11fb965c40947adcd63580bfc4f6cba40e1386ec
hyunduk <hyundukseo@naver.com>
2022-07-10 15:46:19 +0900

    dp 합분해 해결

--- dp/2225_2.py
+++ dp/2225_2.py
@@ -1,10 +1,15 @@
 import sys
 
+MOD = 1000000000
 n, k = map(int, sys.stdin.readline().split())
-dp_table = [0] * (n + 1)
-dp_table[1], dp_table[2] = 1, 3
-for i in range(3, n + 1):
-    dp_table[i] = dp_table[i - 1] + dp_table[i - 2]
-    if i == k:
-        print(dp_table[k])
-        break
+dp_table = [[0] * (n + 1) for _ in range(k + 1)]
+dp_table[1][0] = 1
+for i in range(k + 1):
+    for j in range(n + 1):
+        if i != 0 and j == 0:
+            dp_table[i][j] = 1
+for i in range(1, k + 1):
+    for j in range(1, n + 1):
+        dp_table[i][j] = (dp_table[i - 1][j] + dp_table[i][j - 1]) % MOD
+
+print(dp_table[-1][-1])

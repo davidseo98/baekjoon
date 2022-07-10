3e9f00d496a73a612198435abdc3c484b20d30b4
hyunduk <hyundukseo@naver.com>
2022-07-10 16:46:21 +0900

    dp 동전 1 해결

---
+++ dp/2293.py
@@ -0,0 +1,12 @@
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+num_list = [int(sys.stdin.readline()) for _ in range(n)]
+dp = [0] * (k + 1)
+dp[0] = 1
+
+for num in num_list:
+    for i in range(num, k + 1):
+        if i >= num:
+            dp[i] += dp[i - num]
+print(dp[k])

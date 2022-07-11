43fd287651f9f23dc90a2bb7998a8b0d7cdbb6cd
hyunduk <hyundukseo@naver.com>
2022-07-11 15:03:23 +0900

    dp 가장 긴 증가하는 부분 수열 5 해결

--- dp/14003.py
+++ dp/14003.py
@@ -8,17 +8,17 @@ dp = [num_list[0]]
 dp_2 = [0] * n
 for i in range(n):
     if num_list[i] > dp[-1]:
-        dp.append(num_list[i])
         dp_2[i] = len(dp)
+        dp.append(num_list[i])
+
     else:
         dp_2[i] = bisect.bisect_left(dp, num_list[i])
         dp[dp_2[i]] = num_list[i]
-print(dp_2)
+
 max_dp = max(dp_2)
 max_dp_idx = dp_2.index(max_dp)
 lis = list()
 for i in range(n - 1, -1, -1):
-    print(max_dp)
     if dp_2[i] == max_dp:
         lis.append(num_list[i])
         max_dp -= 1

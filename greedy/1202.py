2d866f9f2b02bb4228a4d435839c73f53cff479f
hyunduk <hyundukseo@naver.com>
2022-07-21 11:47:18 +0900

    greedy 보석 도둑 미해결

--- greedy/1202.py
+++ greedy/1202.py
@@ -1,40 +1,20 @@
 import sys
-
-
-def is_possible(ruby, mid):
-    global bag_list
-    if ruby <= bag_list[mid]:
-        return 1
-    return 0
-
+import heapq
 
 n, k = map(int, sys.stdin.readline().split())
-ruby_list = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+ruby_list = [tuple(map(int, sys.stdin.readline().split())) for _ in range(n)]
 bag_list = [int(sys.stdin.readline()) for _ in range(k)]
-
-ruby_list.sort()
-ruby_list.sort(key=lambda x: x[1], reverse=True)
 bag_list.sort()
-print(ruby_list)
+# heapq.heapify(ruby_list)
 answer = 0
-for ruby in ruby_list:
-    if len(bag_list) == 0:
-        break
-    if ruby[0] > bag_list[-1]:
-        continue
-    else:
-        result = 0
-        lo = 0
-        hi = len(bag_list) - 1
-        while lo <= hi:
-            mid = (lo + hi) // 2
-            if is_possible(ruby[0], mid):
-                result = mid
-                hi = mid - 1
-            else:
-                lo = mid + 1
-        print(bag_list[mid], ruby[1])
-        bag_list.pop(mid)
-        answer += ruby[1]
-
+for bag in bag_list:
+    candidate = list()
+    while ruby_list and ruby_list[0][0] <= bag:
+        w, v = heapq.heappop(ruby_list)
+        heapq.heappush(candidate, (-v, w))
+    if candidate:
+        answer -= heapq.heappop(candidate)[0]
+        while candidate:
+            v, w = heapq.heappop(candidate)
+            heapq.heappush(ruby_list, (w, -v))
 print(answer)

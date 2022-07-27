a471b48db08b245d8ad2d97ff581e3192533b554
hyunduk <hyundukseo@naver.com>
2022-07-27 11:25:03 +0900

    dp 1, 2, 3 더하기

--- dp/9095.py
+++ dp/9095.py
@@ -1,6 +1,11 @@
 import sys
 
+dp = [0] * (12)
+dp[1], dp[2], dp[3] = 1, 2, 4
+for i in range(4, 12):
+    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+
 t = int(sys.stdin.readline())
-for _ in range(t) :
+for _ in range(t):
     num = int(sys.stdin.readline())
-    
+    print(dp[num])

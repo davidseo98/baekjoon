e27d6e89a62df793a857a2f7932834603269eee3
hyunduk <hyundukseo@naver.com>
2022-08-18 09:25:32 +0900

    2206 벽 부수고 이동하기 미해결

--- graph/bfs/2206.py
+++ graph/bfs/2206.py
@@ -6,22 +6,31 @@ def bfs():
     visited = [[False] * n for _ in range(m)]
     queue = deque()
     queue.append((0, 0, False, 0))
-    visited[0][0] = True
+    visited[0][0] = "pass"
     dx = [0, 0, 1, -1]
     dy = [1, -1, 0, 0]
     while queue:
+        # for _ in range(10):
         x, y, did_break, cnt = queue.popleft()
+        # print(x, y, did_break, cnt)
         if x == m - 1 and y == n - 1:
             answer.append(cnt)
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
-            if -1 < nx < m and -1 < ny < n and not visited[nx][ny]:
+            if -1 < nx < m and -1 < ny < n:
                 if graph[nx][ny] == 0:
-                    visited[nx][ny] = True
-                    queue.append((nx, ny, did_break, cnt + 1))
-                if not did_break and graph[nx][ny] == 1:
-                    visited[nx][ny] = True
+                    if visited[x][y] == "pass" and visited[nx][ny] != "pass":
+                        visited[nx][ny] = visited[x][y]
+                        queue.append((nx, ny, did_break, cnt + 1))
+                    elif visited[x][y] == "break" and not visited[nx][ny]:
+                        visited[nx][ny] = visited[x][y]
+                        queue.append((nx, ny, did_break, cnt + 1))
+                if not did_break and graph[nx][ny] == 1 and not visited[nx][ny]:
+                    visited[nx][ny] = "break"
                     queue.append((nx, ny, True, cnt + 1))
+        # print()
+        # for line in visited:
+        #     print(line)
 
 
 m, n = map(int, sys.stdin.readline().split())

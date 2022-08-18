51960357c459f4740a6a45ee94f664652d7d3afd
hyunduk <hyundukseo@naver.com>
2022-08-18 09:37:30 +0900

    1600 말이 되고 싶은 원숭이 미해결

--- graph/bfs/1600.py
+++ graph/bfs/1600.py
@@ -4,14 +4,12 @@ from collections import deque
 k = int(sys.stdin.readline())
 w, h = map(int, sys.stdin.readline().split())
 graph = [list(map(int, sys.stdin.readline().split())) for _ in range(h)]
-visited = [[0] * w for _ in range(h)]
-visited2 = [[False] * w for _ in range(h)]
-
+visited = [[[False] * w for _ in range(h)] for _ in range(k + 1)]
 dx = [1, -1, 0, 0, 2, 2, -2, -2, 1, 1, -1, -1]
 dy = [0, 0, 1, -1, 1, -1, 1, -1, 2, -2, 2, -2]
 queue = deque()
 queue.append((0, 0, 0, k))
-visited[0][0] = 1
+visited[0][0][0] = True
 answer = list()
 while queue:
     x, y, cnt, ck = queue.popleft()
@@ -19,8 +17,13 @@ while queue:
         answer.append(cnt)
     for i in range(4):
         nx, ny = x + dx[i], y + dy[i]
-        if 0 <= nx < h and 0 <= ny < w and visited[nx][ny] != 3 and graph[nx][ny] == 0:
-            visited[nx][ny] = 1
+        if (
+            0 <= nx < h
+            and 0 <= ny < w
+            and not visited[k - ck][nx][ny]
+            and graph[nx][ny] == 0
+        ):
+            visited[k - ck][nx][ny] = True
             queue.append((nx, ny, cnt + 1, ck))
     if ck > 0:
         for i in range(4, 12):
@@ -28,14 +31,14 @@ while queue:
             if (
                 0 <= nx < h
                 and 0 <= ny < w
-                and visited[nx][ny] < 3
+                and not visited[k - ck + 1][nx][ny]
                 and graph[nx][ny] == 0
             ):
-                visited[nx][ny] += 1
+                visited[k - ck + 1][nx][ny] = True
                 queue.append((nx, ny, cnt + 1, ck - 1))
-    print()
-    for line in visited:
-        print(line)
+    # print()
+    # for line in visited:
+    #     print(line)
 if answer:
     print(min(answer))
 else:

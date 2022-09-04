c936728ce60632b19ad7ec6282351ed3225e449d
hyunduk <hyundukseo@naver.com>
2022-09-05 08:02:10 +0900

    수학 fly me to 해결

--- math/1011.py
+++ math/1011.py
@@ -1,22 +1,25 @@
-from re import S
 import sys
 
 
 def find_least_cnt(s, f):
-    target = f - s
-    num = 1
-    answer = float("inf")
+    target = f - s  # 이동해야 하는 거리
+    threshold = 1  # 임계값
+    addition = 1  # 추가하는 값
+    answer = 1  # 최소 이동 횟수
     while 1:
-        cur_val = num * (num + 1)
-        diff = target - cur_val
-        if cur_val > target:
+        if target <= threshold:
             break
-        answer = min(answer, 2 * num + diff)
-        num += 1
-    print(answer)
+        if answer % 2 == 0:
+            threshold += addition
+            answer += 1
+        else:
+            threshold += addition
+            addition += 1
+            answer += 1
+    return answer
 
 
 n = int(sys.stdin.readline())
 for _ in range(n):
     start, finish = map(int, sys.stdin.readline().split())
-    find_least_cnt(start, finish)
+    print(find_least_cnt(start, finish))

317086d2803a00e90dca6308d813ad0c899383c8
hyunduk <hyundukseo@naver.com>
2022-09-10 16:38:52 +0900

    dp 동전2 해결

--- dp/2294.py
+++ dp/2294.py
@@ -2,14 +2,13 @@ import sys
 
 n, k = map(int, sys.stdin.readline().split())
 num_list = [int(sys.stdin.readline()) for _ in range(n)]
-dp = [0] * (k + 1)
-count = [1] * (k + 1)
-dp[0], count[0] = 1, 1
+dp = [10001] * (k + 1)
+dp[0] = 0
+for i in range(len(num_list)):
+    for j in range(num_list[i], k + 1):
+        dp[j] = min(dp[j], dp[j - num_list[i]] + 1)
 
-for num in num_list:
-    for i in range(num, k + 1):
-        if i >= num:
-            dp[i] += dp[i - num]
-            count[i] = min(count[i], count[i - num] + 1)
-print(dp)
-print(count)
+if dp[-1] != 10001 :
+    print(dp[-1])
+else :
+    print(-1)

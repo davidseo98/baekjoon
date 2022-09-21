4c03487cd0e72c4cc7b2a3b3442390df50ca594e
hyunduk <hyundukseo@naver.com>
2022-09-21 21:32:08 +0900

    greedy 보석 도둑 해결

--- greedy/1202.py
+++ greedy/1202.py
@@ -2,19 +2,20 @@ import sys
 import heapq
 
 n, k = map(int, sys.stdin.readline().split())
-ruby_list = [tuple(map(int, sys.stdin.readline().split())) for _ in range(n)]
+ruby_list = list()
+for _ in range(n):
+    heapq.heappush(ruby_list, list(map(int, sys.stdin.readline().split())))
+
 bag_list = [int(sys.stdin.readline()) for _ in range(k)]
 bag_list.sort()
-# heapq.heapify(ruby_list)
+
 answer = 0
+candidate = list()
 for bag in bag_list:
-    candidate = list()
-    while ruby_list and ruby_list[0][0] <= bag:
+    while ruby_list and bag >= ruby_list[0][0]:
         w, v = heapq.heappop(ruby_list)
-        heapq.heappush(candidate, (-v, w))
+        heapq.heappush(candidate, -v)
     if candidate:
-        answer -= heapq.heappop(candidate)[0]
-        while candidate:
-            v, w = heapq.heappop(candidate)
-            heapq.heappush(ruby_list, (w, -v))
+        answer -= heapq.heappop(candidate)
+
 print(answer)

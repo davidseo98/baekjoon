7d96bc7934dc37729fabbfd8cc94a8db675ec8cb
hyunduk <hyundukseo@naver.com>
2022-11-17 08:51:46 +0900

    쉬운 계단 수 해결

--- dp/10844.py
+++ dp/10844.py
@@ -1,18 +1,22 @@
+"""
+백트래킹 -> 시간초과
+다이나믹 프로그래밍 -> 실패 
+"""
+
 import sys
 
 n = int(sys.stdin.readline())
+dp = [[0] * 10 for _ in range(n)]
+for i in range(1, 10):
+    dp[0][i] = 1
 
-limit = 1000000000
-dp = [[0] * (n + 1) for _ in range(12)]
+for i in range(1, n):
+    for j in range(0, 10):
+        if j == 0:
+            dp[i][j] = dp[i - 1][j + 1]
+        elif j == 9:
+            dp[i][j] = dp[i - 1][j - 1]
+        else:
+            dp[i][j] = dp[i - 1][j + 1] + dp[i - 1][j - 1]
 
-for i in range(1, n + 1):
-    for j in range(1, 11):
-        if i == 1:
-            if 2 <= j <= 10:
-                dp[j][i] = 1
-            continue
-        dp[j][i] = dp[j - 1][i - 1] + dp[j + 1][i - 1]
-answer = 0
-for i in range(1, 12):
-    answer += dp[i][-1]
-print(answer)
+print(sum(dp[-1])%1000000000)

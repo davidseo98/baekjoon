ebc9840659c31dcdb18ab3c5fea5804eafda8f40
hyundukseo <hyundukseo98@gmail.com>
2022-11-27 00:54:04 +0900

    문제 해결

--- graph/11048.py
+++ graph/11048.py
@@ -1,29 +1,15 @@
 import sys
 from collections import deque
 
-def bfs():
-    global answer
-
-    queue = deque([(0, 0, graph[0][0])])
-    while queue:
-        cx, cy, cs = queue.popleft()
-        if cx == r - 1 and cy == c - 1:
-            if cs > answer: answer = cs
-
-        for i in range(3):
-            nx, ny = cx + dx[i], cy + dy[i]
-            if 0 <= nx < r and 0 <= ny < c:
-                queue.append((nx, ny, cs + graph[nx][ny]))
-            
-
 r, c = map(int, sys.stdin.readline().split())
 graph = [list(map(int, sys.stdin.readline().split())) for _ in range(r)]
-visited = [[False] * c for _ in range(r)]
-dx = [1, 0, 1]
-dy = [0, 1, 1]
-s = [0, 0, graph[0][0]]
-answer = 0
+dp_table = [[0]*c for _ in range(r)]
+dp_table[0][0] = graph[0][0]
+for i in range(r):
+    for j in range(c):
+        
+        if 0 < i: dp_table[i][j] = max(dp_table[i][j], graph[i][j] + dp_table[i - 1][j])
+        if 0 < j: dp_table[i][j] = max(dp_table[i][j], graph[i][j] + dp_table[i][j - 1])
+        if 0 < i and 0 < j : dp_table[i][j] = max(dp_table[i][j], graph[i][j] + dp_table[i - 1][j - 1])
 
-bfs()
-
-print(answer)
+print(dp_table[-1][-1])

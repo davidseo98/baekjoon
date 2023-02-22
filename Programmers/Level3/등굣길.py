063a5bbe427ace346dc866cfc5ce1b279762525d
hyundukseo <hyundukseo98@gmail.com>
2023-02-22 16:01:41 +0900

    등굣길 해결

---
+++ Programmers/Level3/등굣길.py
@@ -0,0 +1,13 @@
+def solution(m, n, puddles):
+    answer = 0
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    dp[1][1] = 1
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if [j, i] in puddles or (i == 1 and j == 1): continue
+            
+            if i == 1: dp[i][j] = dp[i][j - 1] % 1000000007
+            elif j == 1: dp[i][j] = dp[i - 1][j] % 1000000007
+            else: dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000007
+
+    return dp[n][m]

f4b102c718ede588b870dbf1cddbaae701ebf8ed
hyunduk <hyundukseo@naver.com>
2023-04-13 09:23:57 +0900

    강의실 해결

--- Implementation/14500.py
+++ Implementation/14500.py
@@ -4,43 +4,39 @@ from collections import deque
 def check_all(x, y):
     global answer
 
-    for dxs, dys in zip(total_dxs, total_dys):
-        for dx, dy in zip(dxs, dys):
-            score = board[x][y]
-            for i in range(3):
-                nx, ny = x + dx[i], y + dy[i]
+    for i, diffs in enumerate(zip(total_dxs, total_dys)):
+        dxs, dys = diffs
+        if i == 0: r = 2
+        elif i == 1: r = 1
+        else: r = 4
+
+        for rotate in range(r): # rotate 0 ~ 3
+            cnt = 0
+            score = board[x][y] # 현재 칸 점수 
+
+            for dx, dy in zip(dxs, dys):
+
+                for _ in range(rotate): # 도형 회전 
+                    dx, dy = -dy, dx
+
+                nx, ny = x + dx, y + dy
+
                 if 0 <= nx < n and 0 <= ny < m:
+                    cnt += 1
                     score += board[nx][ny]
 
-            answer = max(answer, score)
+                else: break
+                
+            if cnt == 3: answer = max(answer, score) # 모든 칸이 올려졌을 때만 반영
+
 
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(n)]
 
-total_dxs = []
-total_dys = []
-
-# 정사각형
-total_dxs.append([[0, 1, 1]])
-total_dys.append([[1, 0, 1]])
-
-# 직선 모양
-total_dxs.append([[1, 2, 3], [0, 0, 0]])
-total_dxs.append([[0, 0, 0], [1, 2, 3]])
-
-# ㅗ 모양
-total_dxs.append([[0, 0, -1], [1, 2, 1,], [0, 0, 1], [-1, -2, -1]])
-total_dys.append([[1, 2, -1], [0, 0, 1], [-1, -2, -1], [0, 0, -1]])
-
-# ㄴ 모양 (flip 필요)
-total_dxs.append([[1, 2, 2], [1, 0, 0], [0, 1, 1], [1, 1, 1], [1, 2, 2], [1, 0, 0], [0, 1, 1], [1, 2, 2]])
-total_dys.append([[0, 0, 1], [0, 1, 2], [1, 1, 2], [0, 1, 2], [0, 0, -1], [0, -1, -2], [-1, -1, -2], [0, -1, -2]])
-
-# ㄹ 모양 (flip 필요)
-total_dxs.append([[1, 1, 2], [0, -1, -1], [1, 1, 2], [0, -1, -1]])
-total_dys.append([[0, 1, 1], [1, 1, 2], [0, -1, -1], [-1, -1, -2]])
+total_dxs = [[0, 0, 0], [0, 1, 1], [0, 0, 1], [-1, 0, 1], [-1, 0, 1], [1, 2, 2], [1, 2, 2]]
+total_dys = [[1, 2, 3], [1, 0, 1], [1, 2, 1], [1, 1, 0], [-1, -1, 0], [0, 0, 1], [0, 0, -1]]
 
 answer = -1
 
